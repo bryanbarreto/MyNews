0ec64cfdb409a5e71581d8973fc7980170fa63f0
Bryan Barreto <bryan.barreto@icloud.com>
2021-08-29 16:00:17 -0300

    create unit test bundle

---
+++ MyNewsTests/MyNewsTests.swift
@@ -0,0 +1,31 @@
+//
+//  MyNewsTests.swift
+//  MyNewsTests
+//
+//  Created by Bryan Barreto on 29/08/21.
+//
+
+import XCTest
+
+class MyNewsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+       XCTAssertTrue(true)
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
